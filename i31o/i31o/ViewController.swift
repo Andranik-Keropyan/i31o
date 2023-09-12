26b04d490bba9961dee3852adf94c4f829c745b0
Andranik-Keropyan <111062859+Andranik-Keropyan@users.noreply.github.com>
2023-09-12 21:44:22 +0400

    add programitacly label

--- i31o/i31o/ViewController.swift
+++ i31o/i31o/ViewController.swift
@@ -6,13 +6,37 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private lazy var newText: UILabel = {
+        let text = UILabel()
+        text.text = "some text"
+        
+        return text
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("hello git!")
-        view.backgroundcolor = .red
+        view.backgroundColor = .red
+        view.addSubview(newText)
+        makeconstraints()
+    }
+    
+    func makeconstraints() {
+        newText.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.top.equalToSuperview().offset(20)
+
+
+        }
+        
+        
     }
 
 
